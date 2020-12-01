d2d5a104ad569949960eec6e86ba0ab7fb61d4f5
iftikhar1986 <iftikhar.uddin.fazal@gmail.com>
2020-12-01 18:15:54 +0300

    first commit

---
+++ 02basic/comments.php
@@ -0,0 +1,18 @@
+<?php
+// This is a comment that is ignored by PHP
+
+# This is also a comment
+
+$firstName = 'Iftikhar';  // This stores my first name
+
+$lastName     =
+
+
+    'Uddin'; # This stores my last name
+
+/* This is a comment that spans multiple lines.
+   It uses the same syntax as CSS. */
+
+/*echo $firstName;
+echo ' ';*/
+echo $lastName;
---
+++ 02basic/display_values.php
@@ -0,0 +1,10 @@
+<?php
+$firstName = 'Iftikhar';
+$prénom = 'Uddin';
+//$first-Name = 'David';
+//$1stName = 'David';
+$number = 4200.99;
+
+//echo $number, ' ', $firstName;
+//print $number;
+echo $prénom;
